045bddba9c1a66f08dca9b9a9eb9c07e45ecf0ba
janggt17 <janggt17@gmail.com>
2022-12-13 14:09:48 +0900

    first commit

---
+++ kotlin/grammar/basic/app/src/main/java/com/ta2gi/basic/basic.kt
@@ -0,0 +1,19 @@
+package com.ta2gi.basic
+
+fun main() {
+    // 1. 주석
+    // 한 줄 주석
+    /*
+        여러 줄 주석
+     */
+
+    // 2. 출력
+    print("안녕하세요")
+    println("반갑습니다")
+    print("안녕히계세요")
+    println("")
+
+    // 3. 세미콜론론
+    println("세미콜론O");
+    println("세미콜론X")
+}
