e183ba47ccb0b8ad1a083781546a83229caabd3b
janggt17 <janggt17@gmail.com>
2023-01-05 19:15:52 +0900

    2023.01.05

--- kotlin/project/memo/app/src/main/java/com/ta2gi/memo/MainActivity.kt
+++ kotlin/project/memo/app/src/main/java/com/ta2gi/memo/MainActivity.kt
@@ -2,10 +2,16 @@ package com.ta2gi.memo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ta2gi.memo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
